59a11ff6de4f46f7246281e31d03a78c04972804
EnriqueSFlor <enriquesilvaflor@gmail.com>
2023-02-06 21:31:38 -0300

    finalizando projeto

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não é inválido");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
